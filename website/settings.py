bd41cfc73164b9765b22d5ac501dd018729305bd
Annette Zhao <shuqingz777@gmail.com>
2020-03-23 12:26:01 +0100

    set static root

--- website/settings.py
+++ website/settings.py
@@ -127,16 +127,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-
-LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
-LOGIN_REDIRECT_URL = 'main_page'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, "static/assets"),
-]
+)
