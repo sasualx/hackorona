58068b228e81ad190fd9212157d60a2ca909bdcc
Annette Zhao <shuqingz777@gmail.com>
2020-03-23 11:58:27 +0100

    set static root

--- website/settings.py
+++ website/settings.py
@@ -130,8 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, '/static/')
-
-# STATIC_ROOT = os.path.join(BASE_DIR, '/static_root/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static_root/')
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
